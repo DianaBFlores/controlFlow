b5e1251112e98bc38860853a388cb7837c7d3c98
DianaBFlores <64798817+DianaBFlores@users.noreply.github.com>
2021-04-15 08:10:54 -0400

    updated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,186 +4,84 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 
-class AsciiChars {
-
-    public static void main(String[] args) {
+public class Main {
+public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-
-        int random1 = (int) (Math.random() * 75);
-        int random2 = (int) (Math.random() * 65);
-        Pattern pattern = Pattern.compile("[A-Za-z]*");
-
-        printNumbers();
-        printLowerCase();
-        printUpperCase();
-
-        System.out.println(" ");
-
-        //  questions
-
-        System.out.println("What is your name?");
-        String name = input.nextLine();
-
-        System.out.println("Hello, " + name);
-
-        System.out.println("Would you like to continue? y/n");
-        String answer = input.nextLine().trim();
-
-        while (answer != null) {
-
-            if (answer.equals("y") || answer.equals("Y") || answer.equals("Yes") || answer.equals("yes")) {
-
-                //declarations
-                String redCar = "";
-                String petName = "";
-                int petAge;
-                int lucky = 0;
-                String answerQ = "";
-                int numQB = 0;
-                int carNum = 0;
-                String actorName = "";
-                int randomNum = 0;
+        String userInput = "";
 
 
-                System.out.println("Do you have a red car? (yes/no)");
-                redCar = input.nextLine();
+
+        do {
+        System.out.print("Enter the name of your favorite pet. ");
+        String petName = input.next();
+
+        System.out.println("Enter age of your favorite pet");
+        int petAge = input.nextInt();
+
+        System.out.println("What is your lucky number?");
+        int luckyNum = input.nextInt();
+
+        System.out.println("Do you have a favorite quarterback? If so what is their jersey number?");
+        int jerseyNum = input.nextInt();
+
+        System.out.println("What is two-digit model year of your car?");
+        int carModel = input.nextInt();
+
+        System.out.println("What is the first name of the your favorite actor or actress?");
+        String faveAct = input.next();
+
+        System.out.println("Enter a random number between 1 and 50.");
+        int randomNum = input.nextInt();
 
 
-                System.out.println ("What is the name of a favorite pet?");
-                petName = input.nextLine();
 
-
-                System.out.println("What is the age of a favorite pet?");
-                petAge = input.nextInt();
-
-                System.out.println("What is your lucky number?");
-                lucky = input.nextInt();
-
-                System.out.println("Do you have a favorite quarterback? (yes/no)");
-                answerQ = input.nextLine();
-                answerQ = input.nextLine();
-
-
-                if (answerQ.equals("yes") || answerQ.equals("Yes")) {
-                    System.out.println("What is their jersey number?");
-                    numQB = input.nextInt();
-
-                }
-
-                System.out.println("What is the two-digit model year of your car?");
-                carNum = input.nextInt();
-
-
-                System.out.println("What is the first name of your favorite actor?");
-                actorName = input.nextLine();
-                actorName = input.nextLine();
-
-
-                System.out.println("Enter a random number between 1 and 50.");
-                randomNum = input.nextInt();
-                // System.out.printf("\"%s\" is not a valid response, please enter a number.\n", randomNum);
-                // }
-
-                try {
-                    System.out.println("Calculating your lottery numbers...");
-                    TimeUnit.SECONDS.sleep(2);
-                } catch (InterruptedException e) {
-                    System.err.format("IOException: %s%n", e);
-                }
-
-                int magicBall = 0;
-
-                if (numQB != 0) {
-                    magicBall = numQB;
-                } else {
-                    magicBall = lucky * random1;
-                    while (magicBall > 75) {
-                        magicBall -= 75;
-                    }
-                }
-
-                // Random Lottery Number 1 ; * Use the two digit model year of their car and add
-                // their lucky number to it.
-                int luckNum1 = carNum * lucky;
-                while (luckNum1 > 65) {
-                    luckNum1 -= 65;
-                }
-
-                int luckNum2 = randomNum - random2;
-                if (luckNum2 < 0) {
-                    luckNum2 += random2;
-                }
-
-
-                int luckNum3 = petAge + carNum;
-                if (luckNum3 > 65) {
-                    luckNum3 -= 65;
-                }
-
-                // Random Lottery Number 4 ; Use the first letter of actor first letter.
-                int luckNum4 = actorName.charAt(0);
-                while (luckNum4 > 65) {
-                    luckNum4 -= 65;
-                }
-
-                // Random Lottery Number 5 ; Use the second letter of pet name.
-                int luckNum5 = petName.charAt(1);
-                while (luckNum5 > 65) {
-                    luckNum5 -= 65;
-                }
-
-                // Final Output
-                System.out.println(" ");
-                System.out.println("Lottery Numbers: " + luckNum1 + ", " + luckNum2 + ", " + luckNum3 + ", " + luckNum4 + ", " + luckNum5 + ", "
-                        + "Magic Ball: " + magicBall);
-                System.out.println(" ");
-
-                System.out.println("Do you want to play again??");
-                String cont = input.nextLine();
-                cont = input.nextLine();
-
-                if(cont.equals("No") || cont.equals("no")){
-                    System.out.println("Thank you for playing!");
-                    break;
-                }
-
-            } else {
-                System.out.println("thank you! see you next time");
-                break;
-            }
+        int magicBall = luckyNum * randomNum;
+        if(magicBall > 75) {
+        magicBall = (magicBall % 75) + 1;
         }
 
-        input.close();
-    }
 
-    public static void printNumbers() {
-
-        for (int x = 48; x < 58; x++) {
-            char num = (char) x;
-            System.out.print(num);
+        int lottoNum1 = petName.charAt(2);
+        if(lottoNum1 > 65) {
+        lottoNum1 = (lottoNum1 % 65) + 1;
         }
 
-    }
 
-    public static void printLowerCase() {
-
-        for (int x = 97; x < 123; x++) {
-            char num = (char) x;
-            System.out.print(num);
+        int lottoNum2 = carModel + luckyNum;
+        if(lottoNum2 > 65) {
+        lottoNum2 = (lottoNum2 % 65) + 1;
         }
 
-    }
+        int lottNum3 = 42;
 
-    public static void printUpperCase(){
 
-        for (int x = 65; x < 91; x++) {
-            char num = (char) x;
-            System.out.print(num);
+        int lottoNum4 = faveAct.charAt(0);
+        if(lottoNum4 > 65) {
+        lottoNum4 = (lottoNum4 % 65) + 1;
         }
 
-    }
 
-}
+        int lottoNum5 = faveAct.charAt(faveAct.length() - 1);
+        if(lottoNum5 > 65) {
+        lottoNum5 = (lottoNum5 % 65) + 1;
+        }
+
+
+        System.out.println("Lottery number:" + lottoNum1 + "," + lottoNum2 + "," + lottNum3 + "," + lottoNum4 + "," + lottoNum5);
+        System.out.println("Magic ball:" + magicBall);
+        System.out.println("Do you want to play again? No = n / Yes = y");
+
+        userInput = input.next();
+
+        } while(!userInput.equals("n"));
+
+
+
+
+        }
+
+
+        }
 
 
---
+++ src/com/company/practice.java
@@ -0,0 +1,40 @@
+//package com.company;
+//import java.util.*;
+//
+//public class practice {
+//
+//        public static void main(String[] args) {
+//
+//            Scanner reader = new Scanner(System.in);
+//            System.out.print("Enter three numbers please  ");
+//
+//            double x = reader.nextDouble();
+//            double y = reader.nextDouble();
+//            double z = reader.nextDouble();
+//
+//            if (x >= y){
+//                if (y >= z)
+//                    System.out.print("Numbers from least to greatest are " + z + " "+ y + " " + x);
+//
+//                if  (z >= x)
+//                    System.out.print("Numbers from least to greatest are " + y + " "+ x + " " + z);
+//
+//                if (x > z)
+//                    System.out.print("Numbers from least to greatest are " + y + " " + z + " " + x);
+//            }
+//
+//            if (y > x)
+//            {
+//                if (z >= y)
+//                    System.out.print("Numbers from least to greatest are " + x + " " + y + " "+ z);
+//                if (z >= x)
+//                    System.out.print("numbers from least to greatest are " + y + " " + x + " " + z);
+//                if (x > z)
+//                    System.out.print("Numbers from least to greatest are" + y + " " + z + " " + x);
+//            }
+//
+//
+//        }
+//    }
+
+
